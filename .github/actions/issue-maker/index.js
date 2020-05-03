7d2213d8058c7c3cb15f846f090c40c697366a91
Yoan marchal <marchalyoan@gmail.com>
2020-05-03 11:32:36 +0200

    Update index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -13,7 +13,7 @@ async function run() {
         repo: github.context.repo.repo,
         owner: github.context.repo.owner,
         title: issueTitle,
-        body; jokeBody
+        body: jokeBody
     });
   } catch (err) {
       core.setFailed(err.message);
